f22ce26246c18d5d7c58eb9de347da28e95a629d
theo <teddy101404@outlook.com>
2025-10-21 19:02:30 -0400

    Integrated a discord webhook

--- pythonServer/app.py
+++ pythonServer/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import requests
 from flask_cors import CORS  # Install with: pip install flask-cors
 
 app = Flask(__name__)
@@ -13,6 +14,10 @@ def handle_request(username=None, spell=None):
     print("=======================\n")
     
     if username and spell:
+        webhook_url = "https://discordapp.com/api/webhooks/1430328044152819773/9_sme81Acsj1IaWTkBN7B5GQU_agtShHCW5WtAU4y1MX4JvBmC7Jh4roPvb8oPJlFDBc"
+        data = {"content": f"{username} has casted {spell}!"}
+        response = requests.post(webhook_url, json=data)
+        print(response.status_code)
         return "", 201
     else:
         return "", 204
